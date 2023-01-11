71856857f0d270343c2fbb5a59fd7f6c280c4612
mica <me@limingkai.cn>
2023-01-12 01:30:47 +0800

    feat(cli): 添加新的输出格式

--- apps/cli/src/index.ts
+++ apps/cli/src/index.ts
@@ -95,76 +95,87 @@ Example call:
     list.map((ipOrHost) => check(ipOrHost))
   )
 
-  console.table(
-    results
-      .map((r, index) => {
-        if (r.status === 'fulfilled') {
-          return {
-            ipOrHost: list[index],
-            ...r.value,
-          }
-        } else {
-          return {
-            ipOrHost: list[index],
-            valid: false as const,
-            error: r.reason as unknown,
-          }
-        }
-      })
-      .sort((a, b) => {
-        // 如果两个都是可用的，则响应时间少的排前面
-        if (a.valid && b.valid) {
-          return a.time - b.time
-        }
-
-        // 如果两个都不可用
-        if (!a.valid && !b.valid) {
-          if (
-            ('statusCode' in a && 'statusCode' in b) ||
-            ('error' in a && 'error' in b) ||
-            ('timeout' in a && 'timeout' in b)
-          ) {
-            return 0
-          }
-
-          // status code 不为 200 的排在前面
-          if ('statusCode' in a || 'statusCode' in b) {
-            return 'statusCode' in b ? 1 : -1
-          }
-
-          // 其次是 error
-          if ('error' in a || 'error' in b) {
-            return 'error' in b ? 1 : -1
-          }
-
-          // 最后是超时的
-          if ('timeout' in b || 'timeout' in b) {
-            return 'timeout' in b ? 1 : -1
-          }
-        }
-        if (!a.valid) return 1
-        return -1
-      })
-      .map((v) => {
+  const sorted = results
+    .map((r, index) => {
+      if (r.status === 'fulfilled') {
         return {
-          'IP / Host': v.ipOrHost,
-          Available: v.valid ? 'Yes' : 'No',
-          'Status Code': v.valid
-            ? 200
-            : 'statusCode' in v
-            ? v.statusCode
-            : 'N/A',
-          'Response time': v.valid ? v.time + 'ms' : 'N/A',
-          'Why not available?': v.valid
-            ? 'N/A'
-            : 'statusCode' in v
-            ? 'The status code is not 200.'
-            : 'timeout' in v
-            ? 'No response within 10 seconds.'
-            : 'An error occurred during the request: ' +
-              getErrorMessage(v.error),
+          ipOrHost: list[index],
+          ...r.value,
         }
-      })
+      } else {
+        return {
+          ipOrHost: list[index],
+          valid: false as const,
+          error: r.reason as unknown,
+        }
+      }
+    })
+    .sort((a, b) => {
+      // 如果两个都是可用的，则响应时间少的排前面
+      if (a.valid && b.valid) {
+        return a.time - b.time
+      }
+
+      // 如果两个都不可用
+      if (!a.valid && !b.valid) {
+        if (
+          ('statusCode' in a && 'statusCode' in b) ||
+          ('error' in a && 'error' in b) ||
+          ('timeout' in a && 'timeout' in b)
+        ) {
+          return 0
+        }
+
+        // status code 不为 200 的排在前面
+        if ('statusCode' in a || 'statusCode' in b) {
+          return 'statusCode' in b ? 1 : -1
+        }
+
+        // 其次是 error
+        if ('error' in a || 'error' in b) {
+          return 'error' in b ? 1 : -1
+        }
+
+        // 最后是超时的
+        if ('timeout' in b || 'timeout' in b) {
+          return 'timeout' in b ? 1 : -1
+        }
+      }
+      if (!a.valid) return 1
+      return -1
+    })
+
+  // 生成公告里需要的 markdown 形式
+  // TODO: 可以加个参数来输出这种格式
+  // console.log(
+  //   sorted
+  //     .map((v) => {
+  //       let s = `\`\`\`\n${v.ipOrHost} translate.googleapis.com`
+  //       if (!v.valid) {
+  //         s += '（可能已失效）'
+  //       }
+  //       s += '\n```'
+  //       return s
+  //     })
+  //     .join('\n')
+  // )
+
+  console.table(
+    sorted.map((v) => {
+      return {
+        'IP / Host': v.ipOrHost,
+        Available: v.valid ? 'Yes' : 'No',
+        'Status Code': v.valid ? 200 : 'statusCode' in v ? v.statusCode : 'N/A',
+        'Response time': v.valid ? v.time + 'ms' : 'N/A',
+        'Why not available?': v.valid
+          ? 'N/A'
+          : 'statusCode' in v
+          ? 'The status code is not 200.'
+          : 'timeout' in v
+          ? 'No response within 10 seconds.'
+          : 'An error occurred during the request: ' + getErrorMessage(v.error),
+      }
+    })
   )
 }
 
